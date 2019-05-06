364b75cfcfc0e1c93feef15fac28e21d453444f3
guicong <18883614223@163.com>
2019-05-06 11:38:13 +0800

    添加注释

--- src/main/java/com/cong/config/WebSocketConfig.java
+++ src/main/java/com/cong/config/WebSocketConfig.java
@@ -12,6 +12,10 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 @Configuration
 public class WebSocketConfig {
 
+    /**
+     * 用于扫描和注册所有携带ServerEndPoint注解的实例。
+     * @return
+     */
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
